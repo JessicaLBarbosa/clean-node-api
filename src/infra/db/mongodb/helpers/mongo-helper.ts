2741424c4b6af36ad0e63cf8bc8c520851801f57
Jessica Barbosa <lbarbosa.jessica@gmail.com>
2022-05-11 15:40:37 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { ConnectOptions, MongoClient } from 'mongodb'
+import { Collection, ConnectOptions, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -12,6 +12,9 @@ export const MongoHelper = {
 
   async disconnect(): Promise<void> {
     await this.client.close()
-  }
+  },
 
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  }
 }
